da8f18d50ad38bae0ba1f604b67cee12e591957f
dev-marko <markospasenovski00@gmail.com>
2023-08-26 20:51:09 +0200

    Add new packages

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
-import 'package:flora_edu_mobile/data/constants/environment.dart';
+import 'data/constants/environment.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: Environment.fileName);
