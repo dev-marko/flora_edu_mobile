1e758e25b5450de1ba7840923a65d5f211fb25c2
dev-marko <markospasenovski00@gmail.com>
2023-09-08 19:36:34 +0200

    Implement plant repository & provider

---
+++ lib/data/providers/plant_provider.dart
@@ -0,0 +1,30 @@
+import 'dart:ffi';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/widgets.dart';
+import '../models/plants/plants_list_item.dart';
+
+class PlantProvider {
+  final DatabaseReference _dbRef = FirebaseDatabase.instance.ref('plantsList');
+
+  Future<List<PlantsListItem>> getPlants() async {
+    List<PlantsListItem> plants = [];
+
+    final snapshot = await _dbRef.get();
+
+    if (snapshot.exists && snapshot.value != null) {
+      final plantsMap = snapshot.value as Map;
+      plantsMap.forEach((plantId, data) {
+        plants.add(
+          PlantsListItem(
+            id: plantId,
+            previewImageUrl: Uri.parse(data['previewImageUrl']),
+            plantName: data['plantName'],
+          ),
+        );
+      });
+    }
+
+    return plants;
+  }
+}
