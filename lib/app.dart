a78f08c8b978986b8822d15f443ccc1bf5e89b1d
dev-marko <markospasenovski00@gmail.com>
2023-09-09 16:03:26 +0200

    Remove unused imports

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,3 @@
-import 'package:flora_edu_mobile/data/repositories/plant_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
--- lib/data/providers/plant_provider.dart
+++ lib/data/providers/plant_provider.dart
@@ -1,7 +1,4 @@
-import 'dart:ffi';
-
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/widgets.dart';
 import '../models/plants/plants_list_item.dart';
 
 class PlantProvider {
