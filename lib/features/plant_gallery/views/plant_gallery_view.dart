fcbe3c6f3b2102c757c6de90cd0903a19078853c
dev-marko <markospasenovski00@gmail.com>
2023-09-12 00:39:18 +0200

    Implement GridView for the plant gallery

--- lib/features/plant_gallery/views/plant_gallery_view.dart
+++ lib/features/plant_gallery/views/plant_gallery_view.dart
@@ -23,16 +23,12 @@ class _PlantGalleryViewState extends State<PlantGalleryView> {
         appBar: const FloraEduAppBar(),
         drawer: const FloraEduAppDrawer(),
         floatingActionButton: FloatingActionButton(
-          onPressed: () => Navigator.of(context)
-              .push(AddPlantPictureScreen.route())
-              .then((value) => _refresh()),
+          onPressed: () => Navigator.of(context).push(AddPlantPictureScreen.route()).then((value) => _refresh()),
           child: const Icon(Icons.add),
         ),
         body: FutureBuilder(
-          future: Provider.of<PlantGalleryProvider>(context, listen: false)
-              .getPlants(),
-          builder: (ctx, snapshot) => snapshot.connectionState ==
-                  ConnectionState.waiting
+          future: Provider.of<PlantGalleryProvider>(context, listen: false).getPlants(),
+          builder: (ctx, snapshot) => snapshot.connectionState == ConnectionState.waiting
               ? const Center(
                   child: CircularProgressIndicator(),
                 )
@@ -40,19 +36,75 @@ class _PlantGalleryViewState extends State<PlantGalleryView> {
                   child: const Center(
                     child: Text('Вашата галерија од растенија е празна.'),
                   ),
-                  builder: (ctx, plantPictures, child) => plantPictures
-                          .items.isEmpty
+                  builder: (ctx, plantPictures, child) => plantPictures.items.isEmpty
                       ? child!
-                      : ListView.builder(
-                          itemBuilder: (ctx, index) => ListTile(
-                            key: Key(plantPictures.items[index].id),
-                            leading: CircleAvatar(
-                              backgroundImage:
-                                  FileImage(plantPictures.items[index].image),
-                            ),
-                            title: Text(plantPictures.items[index].name),
+                      : GridView.builder(
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
                           ),
                           itemCount: plantPictures.items.length,
+                          itemBuilder: (context, index) {
+                            return GridTile(
+                              child: InkWell(
+                                onTap: () => showDialog<String>(
+                                  context: context,
+                                  builder: (BuildContext context) => Dialog(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Container(
+                                              width: 450,
+                                              height: 450,
+                                              decoration: BoxDecoration(
+                                                borderRadius: const BorderRadius.all(
+                                                  Radius.circular(10.0),
+                                                ),
+                                                image: DecorationImage(
+                                                  fit: BoxFit.fill,
+                                                  alignment: FractionalOffset.topCenter,
+                                                  image: FileImage(
+                                                    plantPictures.items[index].image,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(height: 7.0),
+                                          Text(plantPictures.items[index].name),
+                                          const SizedBox(height: 7.0),
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: Text(
+                                              'Close',
+                                              style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      fit: BoxFit.fitWidth,
+                                      alignment: FractionalOffset.topCenter,
+                                      image: FileImage(
+                                        plantPictures.items[index].image,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
                         ),
                 ),
         ));
